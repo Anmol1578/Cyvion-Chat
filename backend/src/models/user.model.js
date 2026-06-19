081bf871df9cf9aeb97c3f40771fa6fbd61c0dbb
Anmol1578 <anmol10562@gmail.com>
2026-06-19 21:54:22 +0530

    Implemented User_model and Message_model file

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  fullName: {
+    type: String,
+    required: true,
+  },
+
+  profilePic: {
+    type: String,
+    default: "",
+  },
+}, {timestamps:true});  // createdAt, updatedAt
+
+
+const User = mongoose.model("User",userSchema)
+
+export default User;
