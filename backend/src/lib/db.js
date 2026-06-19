ee4bf7021744dd80628548e7080ba8db531374f7
Anmol Yadav <anmol10562@gmail.com>
2026-06-19 22:57:44 +0530

    Stablish Mongo Db Connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,24 +1,3 @@
-// import mongoose from "mongoose";
-
-// export async function connectDB() {
-//   try {
-//     const mongoUri = process.env.MONGO_URI;
-
-//     if (!mongoUri) {
-//       throw new Error("MONGO_URI is required");
-//     }
-
-//     const conn = await mongoose.connect(mongoUri)
-
-//     console.log("MongoDB Connected", conn.connection.host);
-//   } catch (error) {
-//     console.log("MongoDB Connection error:", conn.connection.error.message);
-//     process.exit(1);
-//     // 1 MEAN FAILED ,  0  MEAN SUCCESS
-//   }
-// }
-
-
 import mongoose from "mongoose";
 
 export async function connectDB() {
